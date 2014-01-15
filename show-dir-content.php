c0863dc4ff545a9ecc64a3d671e4bc5e8a5c92c1
Nikola Zivkovic <emptyclip.srb@aol.com>
2014-01-15 19:16:36 +0100

    second commit

--- show-dir-content.php
+++ show-dir-content.php
@@ -1,3 +1,12 @@
 <?php
-echo "The current directory is ". getcwd() . ".\n
-";
+echo "The current directory is ". getcwd() .".";
+$scan = scandir(getcwd());
+echo " Its contents are:\n";
+foreach($scan as $single)
+{
+	if (($single == ".") || ($single == ".."))
+	{echo("");}
+	else
+	{echo($single . "\n");}
+}
+?>
