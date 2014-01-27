2ac6bd3c270823e34fafe8c1e844c35c485e2d72
Nikola Zivkovic <emptyclip.srb@aol.com>
2014-01-27 03:24:58 +0100

    third commit

--- show-dir-content.php
+++ show-dir-content.php
@@ -1,12 +1,23 @@
 <?php
+/**
+* KOMENTAR
+*
+* PHP version 5
+*
+* @category Snifovanje
+* @package  Snifsnif
+* @author   Nikola <nikola@trtmrt.com>
+* @license  http://www.trtmrt.com/copyleft/licenca.html Licenca prava
+* @link     http://ovoono.com
+*/
 echo "The current directory is ". getcwd() .".";
 $scan = scandir(getcwd());
 echo " Its contents are:\n";
-foreach($scan as $single)
-{
-	if (($single == ".") || ($single == ".."))
-	{echo("");}
-	else
-	{echo($single . "\n");}
+foreach ($scan as $single) {
+    if (($single == ".") || ($single == "..")) {
+        echo("");
+    } else {
+        echo($single . "\n");
+    }
 }
 ?>
